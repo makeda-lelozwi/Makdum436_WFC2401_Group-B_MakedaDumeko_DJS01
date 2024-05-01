1d11c94476ea49f11b63d618fce0ce8c6c02b5d7
Makeda Lelozwi Dumeko <lelodumeko2@gmail.com>
2024-05-01 13:21:25 +0200

    added logic for errors

--- index.js
+++ index.js
@@ -9,22 +9,25 @@
 
 // Given Parameters
 const vel = 10000; // velocity (km/h) fix: convert to m/s by /3.6
-const acc = 3; // acceleration (m/s^2)
+let acc = 3; // acceleration (m/s^2)
 const time = 3600; // seconds (1 hour)
 const d = 0; // distance (km) fix: final answer will be given as m then convert to km (/1000)
 const fuel = 5000; // remaining fuel (kg)
 const fbr = 0.5; // fuel burn rate (kg/s)
 
+//acc = 3 * 3.6;
+
 // Pick up an error with how the function below is called and make it robust to such errors
 function calcNewVel(acc, vel, time) {
-  //fix: convert acceleration by x3.6 
-  //fix: also the parameters were switched around 
-  //where do i use object destructuring?
-  return vel + acc * 3.6 * time;
+  if (acc === 3) {
+    console.error("Convert acceleration.");
+  } else {
+    return vel + acc * time;
+  }
 }
 
 const d2 = (d + (vel / 3.6) * time) / 1000; //calcultes new distance
-const rf = fuel - fbr * time; //calculates remaining fuel 
+const rf = fuel - fbr * time; //calculates remaining fuel
 let vel2 = calcNewVel(acc, vel, time); //calculates new velocity based on acceleration
 
 console.log(`Corrected New Velocity: ${vel2} km/h`);
