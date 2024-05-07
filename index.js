2ae3c28bb4714cc82e82f5a5a5dee8e61f27d86c
Makeda Lelozwi Dumeko <lelodumeko2@gmail.com>
2024-05-07 18:56:02 +0200

    keeping variables close together

--- index.js
+++ index.js
@@ -3,12 +3,18 @@
 - Replace existing comments in code with own explanations 
  */
 
+//INITIAL VALUES
 const initialVelocityInKmPerHour = 10000; // velocity (km/h) fix: convert to m/s by /3.6
 const accelerationInMetresPerSecond = 3; // acceleration (m/s^2)
 const timeInSeconds = 3600; // seconds (1 hour)
 const initialDistanceInKm = 0; // distance (km) fix: final answer will be given as m then convert to km (/1000)
 const initialFuelAmountInKg = 5000; // remaining fuel (kg)
 const fuelBurnRateInKgPerSecond = 0.5; // fuel burn rate (kg/s)
+
+//FINAL VALUES
+const finalDistanceInKm = (d + (vel / 3.6) * time) / 1000; //calcultes new distance
+const remainingFuelAmountInKg = fuel - fbr * time; //calculates remaining fuel
+let finalVelocityInKmPerHour = calcNewVel(acc, vel, time); //calculates new velocity based on acceleration
 
 // Pick up an error with how the function below is called and make it robust to such errors
 const calcNewVel = (acc, vel, time) => {
@@ -18,10 +24,6 @@ const calcNewVel = (acc, vel, time) => {
     return vel + acc * time;
   }
 };
-
-const finalDistanceInKm = (d + (vel / 3.6) * time) / 1000; //calcultes new distance
-const remainingFuelAmountInKg = fuel - fbr * time; //calculates remaining fuel
-let finalVelocityInKmPerHour = calcNewVel(acc, vel, time); //calculates new velocity based on acceleration
 
 console.log(`Corrected New Velocity: ${vel2} km/h`);
 console.log(`Corrected New Distance: ${d2} km`);
